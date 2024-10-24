b3f9989806c27728328cd8f4b587905e3453f705
Shima Abdollahi <6imabdollahi@gmail.com>
2024-10-24 18:38:34 +0330

    function "collapse" updated to a general version

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -2,14 +2,16 @@
 
 function collapse(e){
     let accordionBtn=e.target;
-    if(!accordionBtn.classList.contains("collapsed")){
+    let accordion=accordionBtn.closest(".accordion");
+
+    if(!accordionBtn.classList.contains("collapsed")){ //if the selected item is the opened one:
         accordionBtn.classList.add("collapsed");
         accordionBtn.parentElement.nextElementSibling.classList.remove("show");
     }
     else{
         //Make sure that the "collapsed" class is added to all accordion buttons
         //(Add it to the only button that hasn't it)
-        const btns=document.getElementsByClassName("accordion__button");
+        const btns=accordion.getElementsByClassName("accordion__button");
         for(let btn of btns){
             if(!(btn.classList.contains("collapsed"))){
                 btn.classList.add("collapsed");
@@ -18,7 +20,7 @@ function collapse(e){
 
         //Make sure that none of the "accordion__collapse" classes has the "show class"
         //remove it from the only one.
-        const acco_collapses=document.getElementsByClassName("accordion__collapse");
+        const acco_collapses=accordion.getElementsByClassName("accordion__collapse");
         for(let acco_col of acco_collapses){
             if(acco_col.classList.contains("show")){
                 acco_col.classList.remove("show");
