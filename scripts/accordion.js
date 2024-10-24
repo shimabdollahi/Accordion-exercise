798a8d24138ac50859dbaa51f3dffa9c3c387e8b
Shima Abdollahi <6imabdollahi@gmail.com>
2024-10-24 18:27:23 +0330

    add function "collapse" for accordion buttons

---
+++ scripts/accordion.js
@@ -0,0 +1,38 @@
+
+
+function collapse(e){
+    let accordionBtn=e.target;
+    if(!accordionBtn.classList.contains("collapsed")){
+        accordionBtn.classList.add("collapsed");
+        accordionBtn.parentElement.nextElementSibling.classList.remove("show");
+    }
+    else{
+        //Make sure that the "collapsed" class is added to all accordion buttons
+        //(Add it to the only button that hasn't it)
+        const btns=document.getElementsByClassName("accordion__button");
+        for(let btn of btns){
+            if(!(btn.classList.contains("collapsed"))){
+                btn.classList.add("collapsed");
+            }
+        }
+
+        //Make sure that none of the "accordion__collapse" classes has the "show class"
+        //remove it from the only one.
+        const acco_collapses=document.getElementsByClassName("accordion__collapse");
+        for(let acco_col of acco_collapses){
+            if(acco_col.classList.contains("show")){
+                acco_col.classList.remove("show");
+            }
+        }
+
+        //remove the "collapsed" class from the selected "accordion__button"
+        accordionBtn.classList.remove("collapsed");
+
+        //add the "show" class to the related "accordion__collapse" class
+        accordionBtn.parentElement.nextElementSibling.classList.add("show");
+    }
+}
+
+for(let accoBtn of document.getElementsByClassName("accordion__button")){
+    accoBtn.onclick=collapse;
+}
